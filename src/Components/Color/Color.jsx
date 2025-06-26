98c268a6d984695a76b32e207a54c3d362f4ed42
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-06-26 14:48:07 +0200

    add copy success message and timout of it

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,7 @@
 import "./Color.css";
 import ColorForm from "./ColorForm";
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Color({
   color,
@@ -18,18 +19,25 @@ export default function Color({
   handleCancelDelete,
 }) {
   const [isEditing, setIsEditing] = useState(false);
-  // const [copyMessage, setCopyMessage] = useState("");
+
+  const [copyMessage, setCopyMessage] = useState(false);
+  useEffect(() => {
+    let timerId = setTimeout(() => {
+      setCopyMessage(false); // so verschwindet die Nachricht, da setCopyMessage true sien muss
+    }, 3000);
+    return () => clearTimeout(timerId); //verhindert das sich timer überschneiden
+  }, [copyMessage]); //wenn copyMessage sich verändert(true/false) wird useEffect ausgeführt
 
   async function CopyToClipboard(hex) {
     console.log(hex);
     try {
       await navigator.clipboard.writeText(hex);
       console.log("copied");
+      setCopyMessage(true);
     } catch (error) {
       console.error("copy failed");
     }
   }
-
   return (
     <div
       className="color-card"
@@ -56,12 +64,17 @@ export default function Color({
         <>
           <h3 className="color-card-headline">{color.hex}</h3>
           <button
-            onClick={() => CopyToClipboard(color.hex)}
+            onClick={() => {
+              CopyToClipboard(color.hex);
+              setCopyMessage(true);
+            }}
             type="button"
             title="copy hex"
           >
             Copy
           </button>
+          {copyMessage && <p>Copied!</p>}
+
           <h4>{color.role}</h4>
           <p>contrast: {color.contrastText}</p>
           <button onClick={() => setIsEditing(true)} type="button" title="Edit">
